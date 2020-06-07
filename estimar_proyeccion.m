01b0867526664864aa94bd2eba973fd7174addf5
vparot <vparot@users.noreply.github.com>
2020-06-07 07:04:16 -0400

    Actualizar hasta 6/6

--- estimar_proyeccion.m
+++ estimar_proyeccion.m
@@ -118,3 +118,78 @@ for itReg = 1:numel(uRegion)-1
     title({'Defunciones por cualquier causa',uRegion{itReg}})
     saveas(gcf,sprintf('out_%02d.png',itReg))
 end
+
+%% datos por semana, por comuna, en regiones seleccionadas
+% for itReg = [6 7 13]
+%     if strcmp(uRegion{itReg},{'XVI Región del Ñuble'})
+%         continue
+%     end
+%     mRegion = strcmp(defs.REGION,uRegion{itReg});
+%     uComunaReg = unique(defs(mRegion,:).COMUNA);
+%     for itCom = 1:numel(uComunaReg)
+%         mComuna = strcmp(defs.COMUNA,uComunaReg{itCom});
+%         allwks = [];
+%         for it = 1:numel(uAnyos)
+%             mAnyo = defs.A_O == uAnyos(it);
+%             if strcmp(uRegion{itReg},{'VIII Región de Concepción'})
+%                 mRegion = mRegion | strcmp(defs.REGION,'XVI Región del Ñuble');
+%             end
+%             mSel = mAnyo & mRegion & mComuna;
+%             totPerWkThisYr = varfun(@sum,defs(mSel,:),'InputVariables','TOTAL','GroupingVariables','WeekOfYear');
+%             totals = zeros(53,1);
+%             totals(totPerWkThisYr.WeekOfYear) = totals(totPerWkThisYr.WeekOfYear) + totPerWkThisYr.sum_TOTAL;
+%             if uAnyos(it) == 2020
+%                 thisWk = find(totPerWkThisYr.WeekOfYear==max(totPerWkThisYr.WeekOfYear));
+%                 if isempty(thisWk)
+%                     % algunas comunas como juan fernandez y primavera no tienen muertes todos los años.
+%                     disp(defs(mRegion & mComuna,:))
+%                     continue
+%                 end
+%                 totals(thisWk) = totals(thisWk)*max(totPerWkThisYr.GroupCount)/totPerWkThisYr(thisWk,:).GroupCount;
+%                 totals(thisWk+1:end) = nan;
+%             end
+%             allwks = [allwks totals]; 
+%         end
+%         if any(sum(~~allwks(:,1:end-1))/53 < .9)
+%             continue
+%         end
+%         avgHistPerWeek = mean(sum(allwks(:,1:end-1)))/52;
+%         if avgHistPerWeek < 1
+%             continue
+%         end
+%         sum(allwks);
+%         % collect first and last week
+% %         allwks(1) = allwks(2); 
+%         allwks(1,2:end) = allwks(1,2:end) + allwks(53:end,1:end-1);
+%         allwks(53:end,:) = [];
+%         % median filtering to minimize weekend and holiday artifacts
+%         allwks(1:thisWk,end) = reducepeaks(allwks(1:thisWk,end));
+%         allwks(1:end,1:end-1) = reducepeaks(allwks(1:end,1:end-1));
+%         % extract trend from first principal component
+%         [u, s, v] = svd(allwks(1:end,1:end-1));
+%         % fit log-linear increase in rates
+%         projmat = ((1:size(v,1)+1)'*[0 1]+[1 0]);
+%         coeffs = projmat(1:end-1,:)\log(v(:,1));
+%         sel = 1:1;
+%         allwksest = abs(u(:,sel)*s(sel,sel)*exp(projmat*coeffs)');
+%  
+%         % make figure
+%         figure windowstyle docked
+%         xax = (1:size(allwks,1))';
+%         ydat = allwksest(:,end) + [-1 1].*std(allwks(:,1:end-1)-allwksest(:,1:end-1),[],2);%/sqrt(size(allwks,2)-1);
+%         xax = xax(:);
+%         ydat = ydat(:);
+%         cli = colormap(lines);
+%         plot(xax,[allwksest(:,end)],'color',cli(1,:),'linewidth',2)
+%         hold on
+%         plot(xax,[allwks(:,end)],'color',cli(2,:),'linewidth',2)
+%         patch([xax; xax(end:-1:1)],[ydat(1:end/2); ydat(end:-1:end/2+1)],cli(1,:),'edgecolor','none','facealpha',.2)
+%         ylim(ylim.*[0 1])
+%         xlim([1 51])
+%         xlabel 'Semana'
+%         ylabel 'Defunciones'
+%         legend({'Proyección ajustada desde 2010',['Defunciones 2020 (hasta ' fecha ')']},'location','se')
+%         title({'Defunciones por cualquier causa',[uRegion{itReg} ', comuna de ' capitalize(uComunaReg{itCom})]})
+%     %     saveas(gcf,sprintf('out_%02d.png',itReg))
+%     end % for Comuna
+% end % for Region
